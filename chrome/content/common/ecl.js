108d64aac13d7ef981f7e0fa158944603f01e5ff
thorikawa <thorikawa@d0d07461-0603-4401-acd4-de1884942a52>
2008-09-22 04:35:53 +0000

    git-svn-id: http://svn.coderepos.org/share/platform/firefox/FireMobileSimulator/trunk/src@19719 d0d07461-0603-4401-acd4-de1884942a52

--- chrome/content/common/ecl.js
+++ chrome/content/common/ecl.js
@@ -1,8 +1,21 @@
-//
-// Escape Codec Library: ecl.js (Ver.041208)
-//
-// Copyright (C) http://nurucom-archives.hp.infoseek.co.jp/digital/
-//
+/* ***** BEGIN LICENSE BLOCK Version: GPL 3.0 ***** 
+ * FireMobileFimulator is a Firefox add-on that simulate web browsers of 
+ * japanese mobile phones.
+ * Copyright (C) 2008  http://nurucom-archives.hp.infoseek.co.jp/digital/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * ***** END LICENSE BLOCK ***** */
 
 EscapeSJIS=function(str){
 	return str.replace(/[^*+.-9A-Z_a-z-]/g,function(s){
